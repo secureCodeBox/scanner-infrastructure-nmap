95487aa1eecb85780ba2e5f9505e9e0e5d2fbcb1
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-09-04 14:12:27 +0200

    Corrected deploy script

--- .travis/deployDockerHub.sh
+++ .travis/deployDockerHub.sh
@@ -3,17 +3,17 @@
 echo "Docker Login"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "Pushing to Dockerhub"
+
 if [[ $TRAVIS_BRANCH =~ ^develop$ ]]
 then
     echo "Develop Build: Pushing develop tag"
     
     echo $(docker tag $REPO:$TAG $REPO:develop)
-    echo $(docker tag $REPO:$TAG $REPO:develop-$TRAVIS_COMMIT)
+    echo $(docker tag $REPO:$TAG $REPO:develop-$TRAVIS_BUILD_NUMBER)
 
     echo $(docker push $REPO:develop)
-    echo $(docker push $REPO:develop-$TRAVIS_COMMIT)
-    
-elif [ -z "$TRAVIS_TAG" ]
+    echo $(docker push $REPO:develop-$TRAVIS_BUILD_NUMBER)
+elif [ "$TRAVIS_BRANCH" = "$TRAVIS_TAG" ]
 then
     echo "Tagged Release: Pushing versioned docker image." 
     echo $(docker tag $REPO:$TAG $REPO:$TRAVIS_TAG)
